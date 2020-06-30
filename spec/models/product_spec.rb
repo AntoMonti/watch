19ac63f1824231c9c0b92ad3975085e0480fc067
AntoMonti <a.matviishyn@hotmail.com>
2020-06-30 21:57:27 +0300

    adding relations between product and related product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 RSpec.describe Product, type: :model do
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:price) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:price) }
+  it { should have_many(:related_products) }
+  it { should have_many(:related) }
 end
