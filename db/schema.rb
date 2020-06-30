435ed938151b1088e19196a80bab874a405bfd4f
AntoMonti <a.matviishyn@hotmail.com>
2020-06-30 19:42:21 +0300

    creating and starting migration for related_products

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_530_212_236) do
+ActiveRecord::Schema.define(version: 2020_06_30_163139) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'brands', force: :cascade do |t|
-    t.string 'title'
-    t.string 'bytitle'
-    t.string 'img'
-    t.string 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "brands", force: :cascade do |t|
+    t.string "title"
+    t.string "bytitle"
+    t.string "img"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.integer 'category_id'
-    t.integer 'brand_id'
-    t.string 'title'
-    t.string 'bytitle'
-    t.text 'content'
-    t.float 'price'
-    t.float 'old_price'
-    t.integer 'status'
-    t.string 'keywords'
-    t.string 'description'
-    t.string 'img', default: 'no_image.jpg'
-    t.integer 'hit', default: 0
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "brand_id"
+    t.string "title"
+    t.string "bytitle"
+    t.text "content"
+    t.float "price"
+    t.float "old_price"
+    t.integer "status"
+    t.string "keywords"
+    t.string "description"
+    t.string "img", default: "no_image.jpg"
+    t.integer "hit", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "related_products", id: false, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "related_id"
+  end
+
 end
