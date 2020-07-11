6566a8a3fa2931102d9ec062b7e18ed6ac59851b
AntoMonti <a.matviishyn@hotmail.com>
2020-07-11 10:19:31 +0300

    creating routes for carts and items

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root to: 'main#index'
   resources :product, only: :show
   resources :category, only: :show
+
+  resource :carts, only: %i[show destroy] do
+    resources :items, only: %i[show create destroy]
+  end
+
   resources :search, only: :index
 end
